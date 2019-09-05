be9140e0388e578304d681bccdbcc681aba4cde5
BennettGardiner <bennettpjgardiner@gmail.com>
2019-09-05 13:45:18 +1000

    new problem ideas

---
+++ partition.py
@@ -0,0 +1,2 @@
+# Given a set of integers, decide whether it can be split into two
+# subsets with the same sum.
---
+++ wordbreak.py
@@ -0,0 +1,2 @@
+# Given a string and a dictionary of words, determine if the string can be
+# broken into a space separated sequence of one or more dictionary words.
