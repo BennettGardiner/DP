097e68f02ce8823f4e7db2fa68256d47411dbc33
BennettGardiner <bennettpjgardiner@gmail.com>
2019-09-05 11:36:12 +1000

    modified to find all solns

--- nonlin.py
+++ nonlin.py
@@ -1,14 +1,32 @@
-# Maximise the function (x1 + 5)(x2 + 1)(x3 + 2) subjec to
-# 3x1 + 2x2 + x3 <= 6, where all variables are non-negative integers
+# Maximise the function (x0 + 5)(x1 + 1)(x2 + 2) subject to
+# 3x0 + 2x1 + x2 <= 6, where all variables are non-negative integers
 
-c = [5, 1, 2]
-d = [3, 2, 1]
-T = 6
+c = [5, 1, 2, 1]
+d = [3, 2, 1, 1]
+T = 8
 
+
+_V = {}
 def V(t,s):
-    if t == 3:
-        return (1, 0)
-    else:
-        return max(((a + c[t]) * V(t + 1, s + d[t] * a)[0], a) for a in range(int((T - s)/ d[t]) + 1))
+    if not (t,s) in _V:
+        if t >= len(c):
+            _V[t,s] = (1, 0)
+        else:
+            _V[t,s] = max(((a + c[t]) * V(t + 1, s + d[t] * a)[0], a) for a in range(int((T - s)/ d[t]) + 1))
+    return _V[t,s]
 
-print(V(2,4))
+
+def find_solutions(t, s):
+    max_val = V(t, s)[0]
+    if t >= len(c):
+        yield []
+    else:
+        for a in range(int((T - s) / d[t]) + 1):
+            if (a + c[t]) * V(t + 1, s + d[t] * a)[0] == max_val:
+                for b in find_solutions(t + 1, s + d[t] * a):
+                    yield [a] + b
+
+
+print(V(0,0))
+for sol in find_solutions(0,0):
+    print(sol)
